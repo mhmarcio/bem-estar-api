b893ffaada0b1463b3526de2117c3c8caf2ad6a0
Marcio Henrique <marcio.fonseca@sead.ma.gov.br>
2024-10-31 11:12:29 -0300

    commit inicial v01

---
+++ src/test/java/br/gov/ma/sead/bem_estar_api/BemEstarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ma.sead.bem_estar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemEstarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
