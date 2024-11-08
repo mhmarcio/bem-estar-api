2f05061a35f1957574c87a5a3e045992fc741cff
Marcio Henrique <marcio.fonseca@sead.ma.gov.br>
2024-11-08 11:35:18 -0300

    campos obrigatorios

--- src/main/java/br/gov/ma/sead/bem_estar_api/medico/DadosCadastroMedicos.java
+++ src/main/java/br/gov/ma/sead/bem_estar_api/medico/DadosCadastroMedicos.java
@@ -1,12 +1,24 @@
 package br.gov.ma.sead.bem_estar_api.medico;
 
 import br.gov.ma.sead.bem_estar_api.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroMedicos(
+        @NotBlank
         String nome,
+        @NotBlank
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotBlank
         String telefone,
+        @NotBlank
         Especialidade especialidade,
+        @NotNull
+        @Valid
         DadosEndereco endereco) {
 }
